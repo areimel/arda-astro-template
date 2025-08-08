7f8d2519de566cccb8c6b690f012ddd875fe27e0
Alec Reimel <alecreimel1@gmail.com>
2025-08-08 16:24:03 -0400

    new careers pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -66,6 +66,26 @@ const productsCollection = defineCollection({
   }),
 });
 
+const careersCollection = defineCollection({
+  schema: z.object({
+    draft: z.boolean(),
+    title: z.string(),
+    snippet: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    publishDate: z.string().transform(str => new Date(str)),
+    author: z.string().default('Astroship'),
+    department: z.string(),
+    location: z.string(),
+    jobType: z.string(),
+    salaryRange: z.string(),
+    experienceLevel: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
@@ -73,4 +93,5 @@ export const collections = {
   'team': teamCollection,
   'case-studies': caseStudiesCollection,
   'products': productsCollection,
+  'careers': careersCollection,
 };
